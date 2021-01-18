8df411de516c0efa38eeae5216af1778ee8f3a86
Sameer Khowaja - SK <54762751+SameerKhowaja@users.noreply.github.com>
2021-01-18 15:33:51 +0500

    schema

---
+++ sql/hms.sql
@@ -0,0 +1,384 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 18, 2021 at 11:32 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `fname` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lname` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_id` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` blob DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `fname`, `lname`, `email_id`, `password`, `image`, `created_at`, `updated_at`) VALUES
+(1, 'Sameer', 'Khowaja', 'sameerkhowaja@gmail.com', 'password', '', '2021-01-18 09:46:24', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointments`
+--
+
+CREATE TABLE `appointments` (
+  `appointment_id` int(15) NOT NULL,
+  `pid` int(15) NOT NULL,
+  `did` int(15) NOT NULL,
+  `appointment_date` date NOT NULL,
+  `appointment_time` time NOT NULL,
+  `confrim` varchar(10) NOT NULL,
+  `confirm_by_rid` int(15) NOT NULL,
+  `current_datetime` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment_result`
+--
+
+CREATE TABLE `appointment_result` (
+  `result_id` int(15) NOT NULL,
+  `appointment_id` int(15) NOT NULL,
+  `prescribe_medicine` varchar(500) NOT NULL,
+  `prescribe_test` varchar(500) NOT NULL,
+  `description` int(11) NOT NULL,
+  `result_time` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `beds`
+--
+
+CREATE TABLE `beds` (
+  `bid` int(15) NOT NULL,
+  `bed_number` varchar(100) NOT NULL,
+  `rid` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctors`
+--
+
+CREATE TABLE `doctors` (
+  `did` int(15) NOT NULL,
+  `primary_id` int(15) NOT NULL,
+  `specialist` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hospital_data`
+--
+
+CREATE TABLE `hospital_data` (
+  `primary_id` int(15) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `fname` int(100) NOT NULL,
+  `lname` int(100) NOT NULL,
+  `cnic` varchar(100) NOT NULL,
+  `email_id` varchar(200) NOT NULL,
+  `gender` varchar(15) NOT NULL,
+  `phone_number` varchar(30) NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `dob` date NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `image` blob DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lab_technicians`
+--
+
+CREATE TABLE `lab_technicians` (
+  `tid` int(15) NOT NULL,
+  `primary_id` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lab_test`
+--
+
+CREATE TABLE `lab_test` (
+  `test_id` int(15) NOT NULL,
+  `pid` int(15) NOT NULL,
+  `did` int(15) NOT NULL,
+  `tid` int(15) NOT NULL,
+  `test_date` date NOT NULL,
+  `test_time` time NOT NULL,
+  `result_date` date NOT NULL,
+  `result_time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lab_test_report`
+--
+
+CREATE TABLE `lab_test_report` (
+  `report_id` int(15) NOT NULL,
+  `test_id` int(15) NOT NULL,
+  `report` blob NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `result_time` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patients`
+--
+
+CREATE TABLE `patients` (
+  `pid` int(15) NOT NULL,
+  `primary_id` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient_addmission`
+--
+
+CREATE TABLE `patient_addmission` (
+  `addmission_id` int(15) NOT NULL,
+  `pid` int(15) NOT NULL,
+  `rid` int(15) NOT NULL,
+  `bid` int(15) NOT NULL,
+  `date_admitted` timestamp NULL DEFAULT NULL,
+  `date_discharged` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receptionists`
+--
+
+CREATE TABLE `receptionists` (
+  `rid` int(15) NOT NULL,
+  `primary_id` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `rid` int(15) NOT NULL,
+  `room_number` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`appointment_id`);
+
+--
+-- Indexes for table `appointment_result`
+--
+ALTER TABLE `appointment_result`
+  ADD PRIMARY KEY (`result_id`);
+
+--
+-- Indexes for table `beds`
+--
+ALTER TABLE `beds`
+  ADD PRIMARY KEY (`bid`);
+
+--
+-- Indexes for table `doctors`
+--
+ALTER TABLE `doctors`
+  ADD PRIMARY KEY (`did`);
+
+--
+-- Indexes for table `hospital_data`
+--
+ALTER TABLE `hospital_data`
+  ADD PRIMARY KEY (`primary_id`);
+
+--
+-- Indexes for table `lab_technicians`
+--
+ALTER TABLE `lab_technicians`
+  ADD PRIMARY KEY (`tid`);
+
+--
+-- Indexes for table `lab_test`
+--
+ALTER TABLE `lab_test`
+  ADD PRIMARY KEY (`test_id`);
+
+--
+-- Indexes for table `lab_test_report`
+--
+ALTER TABLE `lab_test_report`
+  ADD PRIMARY KEY (`report_id`);
+
+--
+-- Indexes for table `patients`
+--
+ALTER TABLE `patients`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `patient_addmission`
+--
+ALTER TABLE `patient_addmission`
+  ADD PRIMARY KEY (`addmission_id`);
+
+--
+-- Indexes for table `receptionists`
+--
+ALTER TABLE `receptionists`
+  ADD PRIMARY KEY (`rid`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`rid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `appointments`
+--
+ALTER TABLE `appointments`
+  MODIFY `appointment_id` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `appointment_result`
+--
+ALTER TABLE `appointment_result`
+  MODIFY `result_id` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `beds`
+--
+ALTER TABLE `beds`
+  MODIFY `bid` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `doctors`
+--
+ALTER TABLE `doctors`
+  MODIFY `did` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hospital_data`
+--
+ALTER TABLE `hospital_data`
+  MODIFY `primary_id` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lab_technicians`
+--
+ALTER TABLE `lab_technicians`
+  MODIFY `tid` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lab_test`
+--
+ALTER TABLE `lab_test`
+  MODIFY `test_id` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lab_test_report`
+--
+ALTER TABLE `lab_test_report`
+  MODIFY `report_id` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `patients`
+--
+ALTER TABLE `patients`
+  MODIFY `pid` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `patient_addmission`
+--
+ALTER TABLE `patient_addmission`
+  MODIFY `addmission_id` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `receptionists`
+--
+ALTER TABLE `receptionists`
+  MODIFY `rid` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `rid` int(15) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
