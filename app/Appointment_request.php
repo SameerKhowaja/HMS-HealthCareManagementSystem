df5fb4acba59cd2cd6b8497e6e7a0492f2f94fe8
Ali rehman <ar5557487@gmail.com>
2021-02-11 20:43:52 +0500

    first

--- app/Appointment_request.php
+++ app/Appointment_request.php
@@ -3,22 +3,26 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Doctor;
+use App\Patient;
 
 class Appointment_request extends Model
 {
     //
     // Set primary key for easy access
     protected $primaryKey = 'appointment_id';
+    protected $fillable = ['doctor_id', 'patient_id'];
 
 
     public function patient()
     {
-        return $this->belongsTo('App\Patient');
+        return $this->belongsTo(Patient::class,'patient_id');
     }
 
     public function doctor()
     {
-        return $this->belongsTo('App\Doctor');
+        return $this->belongsTo(Doctor::class,'doctor_id');
+        
     }
 
 }
--- database/migrations/2021_02_09_153950_create_appointment_requests_table.php
+++ database/migrations/2021_02_09_153950_create_appointment_requests_table.php
@@ -17,11 +17,13 @@ class CreateAppointmentRequestsTable extends Migration
         Schema::create('appointment_requests', function (Blueprint $table) {
             $table->bigIncrements("appointment_id", 20);
             $table->date('appointment_date');
-            $table->string('day',3);
+            $table->string('day',10);
             $table->unsignedBigInteger('patient_id');
             $table->unsignedBigInteger('doctor_id');
             $table->string('Description',400)->nullable();
             $table->boolean('confrim')->default(false);
+            $table->time('start_time');
+            $table->time('end_time');
             $table->foreign('patient_id')->references('patient_id')->on('patients')->onDelete('cascade');
             $table->foreign('doctor_id')->references('doctor_id')->on('doctors')->onDelete('cascade');
             $table->timestamps();
